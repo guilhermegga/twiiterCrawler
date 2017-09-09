6e4e78170daf59742efbc8c0983114364f02663b
guilherme <guilherme@pc-linx>
2017-09-09 19:13:43 -0300

    apenas alguns ajustes na classe main

--- pi-project-2017/src/crawler/PegaDadosTwitter.java
+++ pi-project-2017/src/crawler/PegaDadosTwitter.java
@@ -22,10 +22,11 @@ public class PegaDadosTwitter {
 
 		// set chaves de autorizaçoes para pegar dados
 		config = new ConfigurationBuilder();
-		config.setOAuthConsumerKey("XXXX");
-		config.setOAuthConsumerSecret("XXXX");
-		config.setOAuthAccessToken("XXXXXXXX");
-		config.setOAuthAccessTokenSecret("XXXXX");
+		config.setOAuthConsumerKey("3OKAOOEoPlJ5aQKR3ZII5OQGY");
+		config.setOAuthConsumerSecret("IdfwEWchz8Mv468pklbb06px08lpDKHYHMEGSKpuLWYjtD1pMa");
+		config.setOAuthAccessToken("296608449-X8wgkb5pbreYlU1gcSNBNayJTGuF1SyRIX6zapp3");
+		config.setOAuthAccessTokenSecret("nxKcuaGed9V7kxQhU5DDCy3FrGqcuzwqZgHdvT5Gtb9Cf");
+
 
 		twitterFactory = new TwitterFactory(config.build());
 		tw = twitterFactory.getInstance();
--- pi-project-2017/src/main/Principal.java
+++ pi-project-2017/src/main/Principal.java
@@ -11,11 +11,23 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		CrudDados dao = new CrudDadosImpl();
+			
+		/*configuraçoes do arquivo xml
+		 * consulte classe XmlUtil para ver exemplo do arquivo
+		*/
+		
 		XmlUtil xml = new XmlUtil();
-
+		
+		//qual caminho esta o xml
+		xml.setCaminhoXML("/home/guilherme/eclipse-workspace/tags.xml");
+		
+		//qual tag buscar no xml
+		xml.setTagXml("palavra");
+	
+		//retorna uma lista de palavras do xml
 		List<String> palavrasChave = xml.retornaPalavrasXml();
 
+		CrudDados dao = new CrudDadosImpl();
 		PegaDadosTwitter dadosTwitter = new PegaDadosTwitter();
 
 		for (String s : palavrasChave) {
